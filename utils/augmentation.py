9695db027c5bbfdf8f0d4884a536b6933f54e6b9
应子曦 <1377069805@qq.com>
2025-06-30 18:13:02 +0800

    added augmentation script

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -33,9 +33,20 @@ def augment_lighting_for_image(img_np, scale_range_min=0.3, scale_range_max=1.8,
     img_augmented=img_augmented.astype(np.uint8)
     return img_augmented
 
+def simple_retinex(img, sigma=80):
+    blurred = cv2.GaussianBlur(img, (0, 0), sigma)
+    retinex = cv2.addWeighted(img, 1.0, blurred, -1.0, 128)
+    return retinex
+
 if __name__ == '__main__':
     img = cv2.imread('/media/noematrix/One Touch/AlignAnything_real/25.06.22/hdf5/goal_images/img1/0.png')
-    img_n=img[:,:,::-1].copy()
-    img_aug = augment_lighting_for_image(img_np=img_n)
-    cv2.imshow("img",img_aug)
+    img=img[:,:,::-1].copy()
+    # img_aug = augment_lighting_for_image(img_np=img)
+    cv2.imshow("img",img)
+    cv2.waitKey(0)
+
+    from skimage import exposure
+    # adjusted = exposure.adjust_gam ma(img, gamma=0.5)  # 伽马校正
+    adjusted = simple_retinex(img)
+    cv2.imshow('adjusted', adjusted)
     cv2.waitKey(0)
